302c15ffcf2d672744fd51adad7bc63451c4d455
briankariuki <bryankariuki79@gmail.com>
2020-08-09 14:27:52 +0300

    added top bar

---
+++ flutter_facebook/lib/screens/home_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+import '../config/palette.dart';
+import '../data/data.dart';
+import '../data/data.dart';
+import '../data/data.dart';
+import '../widgets/widgets.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            brightness: Brightness.light,
+            backgroundColor: Colors.white,
+            title: Text(
+              'facebook',
+              style: const TextStyle(
+                  color: Palette.facebookBlue,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: -1.2),
+            ),
+            centerTitle: false,
+            floating: true,
+            actions: [
+              CircleButton(
+                icon: Icons.search,
+                iconSize: 30.0,
+                onPressed: () => print('Search'),
+              ),
+              CircleButton(
+                icon: MdiIcons.facebookMessenger,
+                iconSize: 30.0,
+                onPressed: () => print('Messenger'),
+              ),
+            ],
+          ),
+          SliverToBoxAdapter(
+            child: CreatePostContainer(currentUser: currentUser),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 5.0),
+            sliver: SliverToBoxAdapter(
+              child: Rooms(onlineUsers: onlineUsers),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ flutter_facebook/lib/widgets/rooms.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import '../models/models.dart';
+
+class Rooms extends StatelessWidget {
+  final List<User> onlineUsers;
+
+  const Rooms({
+    Key key,
+    @required this.onlineUsers,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60.0,
+      color: Colors.orange,
+      child: ListView.builder(
+        padding: const EdgeInsets.symmetric(
+          vertical: 10.0,
+          horizontal: 4.0,
+        ),
+        scrollDirection: Axis.horizontal,
+        itemCount: 1 + onlineUsers.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            margin: const EdgeInsets.all(2.0),
+            height: 20.0,
+            width: 20.0,
+            color: Colors.red,
+          );
+        },
+      ),
+    );
+  }
+}
