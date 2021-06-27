e4ca2ae899be0c7f60e7c55f876784b56d87b5a6
briankariuki <briankariukidev@gmail.com>
2021-06-27 09:34:27 +0300

    add images and song data

--- music_app_concept/lib/assets.dart
+++ music_app_concept/lib/assets.dart
@@ -5,4 +5,17 @@ class Assets {
   static const String cog = '${_icons}cog.svg';
 
   static const String ampersand = '${_images}ampersand.jpg';
+  static const String immortal = '${_images}immortal.jpg';
+  static const String flutter = '${_images}flutter.jpg';
+  static const String echoes = '${_images}echoes.jpg';
+  static const String gravity = '${_images}gravity.jpg';
+  static const String inspiration = '${_images}inspiration.jpg';
+  static const String sunset_dreamz = '${_images}sunset dreamz.jpg';
+  static const String good_morning = '${_images}good morning.jpg';
+  static const String the_light = '${_images}the light.jpg';
+  static const String stars_tonight = '${_images}stars tonight.jpg';
+  static const String louder_now = '${_images}louder now.jpg';
+  static const String this_far = '${_images}this far.jpg';
+  static const String like_home = '${_images}like home.jpg';
+  static const String blue_eyed_demon = '${_images}blue eyed demon.jpg';
 }
--- music_app_concept/lib/data/songs.dart
+++ music_app_concept/lib/data/songs.dart
@@ -4,7 +4,72 @@ import 'package:music_app_concept/models/song.dart';
 final List<Song> songs = [
   Song(
     title: 'Ampersand',
-    artist: 'Droeloe',
+    artist: 'AllttA',
     albumUrl: Assets.ampersand,
+  ),
+  Song(
+    title: 'Sunset Dreamz',
+    artist: 'Neon Beach',
+    albumUrl: Assets.sunset_dreamz,
+  ),
+  Song(
+    title: 'Good Morning',
+    artist: 'Laxcity',
+    albumUrl: Assets.good_morning,
+  ),
+  Song(
+    title: 'Immortal',
+    artist: 'Elley Duhe',
+    albumUrl: Assets.immortal,
+  ),
+  Song(
+    title: 'Flutter',
+    artist: 'Diamond Eyes',
+    albumUrl: Assets.flutter,
+  ),
+  Song(
+    title: 'Inspiration',
+    artist: 'Unknown Brain, Aviella',
+    albumUrl: Assets.inspiration,
+  ),
+  Song(
+    title: 'The Light',
+    artist: 'ÉWN',
+    albumUrl: Assets.the_light,
+  ),
+  Song(
+    title: 'Stars Tonight',
+    artist: 'Zeds dead, DROELOE',
+    albumUrl: Assets.stars_tonight,
+  ),
+  Song(
+    title: 'Louder Now',
+    artist: 'Tobu',
+    albumUrl: Assets.louder_now,
+  ),
+  Song(
+    title: 'Echoes',
+    artist: 'LFZ',
+    albumUrl: Assets.echoes,
+  ),
+  Song(
+    title: 'This Far',
+    artist: 'Raven & Kreyn, Nino Lucarelli',
+    albumUrl: Assets.this_far,
+  ),
+  Song(
+    title: 'Like Home',
+    artist: 'Unknown Brain, Trove',
+    albumUrl: Assets.this_far,
+  ),
+  Song(
+    title: 'Gravity',
+    artist: 'Diamond Eyes',
+    albumUrl: Assets.gravity,
+  ),
+  Song(
+    title: 'Blue Eyed Demon',
+    artist: 'ROY KNOX',
+    albumUrl: Assets.blue_eyed_demon,
   ),
 ];
